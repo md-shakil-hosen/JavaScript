77ca9140ac09322cd9862c9ab5df23dfdce1dc76
md-shakil-hosen <shakilinfo91@gmail.com>
2021-07-31 18:23:50 +0600

    Start JavaScript Extra Learning

---
+++ index.js
@@ -0,0 +1,22 @@
+// Start javaScript Practice
+// window.alert(5 + 6);
+// JavaScript Expressions
+
+console.log(5 * 2);
+console.log(5 + 2 * 10);
+console.log(10 / 5);
+console.log(20 * 2 / 5);
+
+// Expressions can also contain variable values:
+
+var x;
+x = 5;
+var valueOfx = x * 5;
+console.log(valueOfx);
+
+// The let Keyword Creates Variables 
+
+let firstName, lastName;
+firstName = "Shakil";
+lastName = "Mahmud";
+console.log(lastName);
