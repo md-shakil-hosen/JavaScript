14de3910357c863fd8272acbc890b8249c1e3d01
md-shakil-hosen <shakilinfo91@gmail.com>
2021-08-01 11:43:31 +0600

    Underscore

--- variables.js
+++ variables.js
@@ -5,3 +5,38 @@ var y = 9;
 var z = x * y;
 console.log(z);
 
+// Much Like Algebra
+// Sum of Algebra 
+
+var CowPrice = 25850;
+var CowCoast = 500;
+var TotalCoast = CowPrice + CowCoast;
+console.log(TotalCoast);
+
+// JavaScript Data Type 
+
+var PI = 3.1416;
+var UserName = "Your User Name";
+var YourAnswer = "Yes I am ";
+var lineBreak = PI + "<br>" + UserName + "<br>" + YourAnswer;
+// console.log(lineBreak);
+
+// Sum 
+var x = 2 + 5 + 6;
+console.log(x);
+
+// dollar sign is treated as a letter in JavaScript names
+var $ = 250;
+var $mayMoney = 50;
+var $TotalMoney = $ + $mayMoney;
+console.log($TotalMoney);
+
+// underscore is treated as a letter in JavaScript names
+
+var _x = 200;
+var _y = 200 + " " + "Taka Na Dele Apner Chakri Thakbi Nah :D";
+var _Name = "Shakil Mahmud";
+var z = _Name + " " + _y;
+console.log(z);
+
+
