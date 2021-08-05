a5dc0c377c75587c0e58cd79d709ff7f9a68885f
md-shakil-hosen <shakilinfo91@gmail.com>
2021-08-05 19:45:37 +0600

    JavaScript

---
+++ Operators.js
@@ -0,0 +1,31 @@
+// JavaScript Operators
+// Addition 
+let x = 5;
+let y = 4;
+let z = x + y;
+
+// JavaScript Multiplication 
+let a = 4;
+let b = 7;
+let c = a * b;
+
+// JavaScript String Operators
+let text1 = 'Shakli';
+let text2 = 'Mahmud';
+let result = text1 + " " + text2;
+
+// add (concatenate)
+let Names = 'What is Your Name?';
+Names += 'My Name is Shakil Mahmud';
+
+// Adding Strings and Numbers
+var StringNumber = 5 + 5;
+var AddStringNumber = "5" + '5';
+
+// Power 
+let Num1 = 5;
+let Power = Math.pow(Num1, 2);
+console.log(Power);
+
+
+
